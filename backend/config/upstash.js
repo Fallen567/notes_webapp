309c884790f981388798446222383ab355a23e66
Fallen567 <hammadasimkayani@gmail.com>
2026-04-27 01:09:13 +0500

    hiding database creds, but the ratelimiter is not working because it cannot properly read the creds from .env

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -1,16 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-// hardcoding creds since .env read not working
 const redis = new Redis({
-  url: "https://infinite-tick-106117.upstash.io",
-  token: "gQAAAAAAAZ6FAAIgcDJiZTE2OGVkMDM0OTQ0MTVkYjVlODdiY2M1ZGYzZjI4Yw" 
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-// limiter that allows 10 requests per 20 seconds
+// 4 requests per 10 seconds
 const ratelimit = new Ratelimit({
-    redis: redis,
-    limiter: Ratelimit.slidingWindow(4,"10 s")
+  redis,
+  limiter: Ratelimit.slidingWindow(4, "10 s"),
 });
 
 export default ratelimit;
