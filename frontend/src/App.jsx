db9b3aeb0b6c8494f4a744b76ceccba77bd3c40a
Fallen567 <hammadasimkayani@gmail.com>
2026-04-27 03:55:21 +0500

    tailwind testing and daisy ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import {Route, Routes} from "react-router";
+import {Route, Routes} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-
+      <Toaster/>
+        <button onClick={()=> toast.error("congrats")} className="text-red-500 p-4 bg-pink-300"> 
+          CLICK ME 
+        </button>
         <Routes>
             <Route path ="/" element={<HomePage />} />
             <Route path ="/create" element={<CreatePage />} />
+            <Route path ="/note/:id" element={<NoteDetailPage />} />
         </Routes>
 
     </div>
